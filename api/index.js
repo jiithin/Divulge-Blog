98ddf91e1faae2c6a6a7248def2dac01d729af68
jiithin <jithijithin9@gmail.com>
2024-10-27 09:00:14 +0530

    -

--- api/index.js
+++ api/index.js
@@ -8,7 +8,6 @@ import postRoutes from './Routes/postRoute.js';
 import commentRoutes from './Routes/commentRoute.js';
 import path from 'path';
 import cookieParser from 'cookie-parser';
-import cors from 'cors';
 
 dotenv.config();
 
@@ -21,7 +20,6 @@ const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({origin: ["https://divulge.onrender.com","https://thedivulge.vercel.app","http://localhost:5173"], credentials: true,}));
 
 app.listen(3000, ()=>{
     console.log('Server is running on port 3000!!');
@@ -35,7 +33,6 @@ app.use('/blog/comment', commentRoutes);
 
 app.use(express.static(path.join(__dirname, '/client/dist')));
 
-//whatever url otherthan user,auth,post,comment its going to run index.html
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
 });
